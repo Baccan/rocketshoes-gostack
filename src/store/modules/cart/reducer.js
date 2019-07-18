98f434b736e9e06d51b6b8a8666c0839b8b23652
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-18 16:33:57 -0300

    Listando produtos no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,12 +1,18 @@
 export default function cart(state = [], action) {
-  console.log(state);
+  // console.log(state);
   // action é a a ação que foi disparada
 
   // state é o estado anterior a alteração do state
 
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
