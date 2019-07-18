4fd678cc3382eb942a10a2e58ce94812b7124cf2
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-18 16:46:11 -0300

    Tratando produtos duplicados

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   // console.log(state);
   // action é a a ação que foi disparada
@@ -6,13 +8,24 @@ export default function cart(state = [], action) {
 
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
+
+    // COM IMUTABILIDADE (sem a lib immer)
+    // return [
+    //   ...state,
+    //   {
+    //     ...action.product,
+    //     amount: 1,
+    //   },
+    // ];
     default:
       return state;
   }
